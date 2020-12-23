2a5433f268cfbd3ae39e5ad6bbeb0460d0e7e52a
Kavyamol <74911379+Kavyamol@users.noreply.github.com>
2020-12-23 19:57:26 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<admin></admin><product></product>',
+  
+})
+export class AppComponent {
+  title = 'angular10';
+}
